bd054de3784ce893483a9ef0cd42b8f17d54b49b
lmixv <111783902+lmixv@users.noreply.github.com>
2025-10-05 20:45:57 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,49 @@
+// sw.js - Service Worker for My Sanrio Life RPG
+const CACHE_NAME = 'my-melody-rpg-v1';
+const urlsToCache = [
+  '/',
+  '/my-melody-rpg.html',
+  '/manifest.json'
+];
+
+// Install event
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Activate event
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
